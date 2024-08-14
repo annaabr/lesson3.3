f110d3d1ab660145b6f0f9948f2f4f3d79edd476
annaabr <annaabramyan2013@gmail.com>
2024-08-14 19:19:12 +0300

    add continuous movement

--- main.py
+++ main.py
@@ -15,8 +15,13 @@ target_img = pygame.image.load("img/target.png")
 target_width = 80
 target_height = 80
 
+# Начальная позиция цели
 target_x = random.randrange(0, SCREEN_WIDTH - target_width)
 target_y = random.randrange(0, SCREEN_HEIGHT - target_height)
+
+# Направление и скорость
+target_dx = random.choice([-1, 1]) * random.uniform(0.03, 0.07)
+target_dy = random.choice([-1, 1]) * random.uniform(0.03, 0.07)
 
 color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
 
@@ -34,8 +39,21 @@ while running:
             mouse_x, mouse_y = pygame.mouse.get_pos()
             if target_x < mouse_x < target_x + target_width and target_y < mouse_y < target_y + target_height:
                 score += 1  # Увеличиваем счетчик попаданий
+                # Перемещение цели в случайную точку
                 target_x = random.randrange(0, SCREEN_WIDTH - target_width)
                 target_y = random.randrange(0, SCREEN_HEIGHT - target_height)
+                target_dx = random.choice([-1, 1]) * random.uniform(0.03, 0.07)
+                target_dy = random.choice([-1, 1]) * random.uniform(0.03, 0.07)
+
+    # Обновляем позицию цели
+    target_x += target_dx
+    target_y += target_dy
+
+    # Проверка выхода за пределы экрана
+    if target_x < 0 or target_x > SCREEN_WIDTH - target_width:
+        target_dx = -target_dx  # Меняем направление по оси X
+    if target_y < 0 or target_y > SCREEN_HEIGHT - target_height:
+        target_dy = -target_dy  # Меняем направление по оси Y
 
     # Отображаем мишень
     screen.blit(target_img, (target_x, target_y))
